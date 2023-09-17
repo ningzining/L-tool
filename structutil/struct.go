b07239e3e4d6b510313e79316cc2b3ce7038886d
liningning <771846387@qq.com>
2023-09-17 16:58:11 +0800

    feature: 添加结构体工具类

---
+++ structutil/struct.go
@@ -0,0 +1,22 @@
+package structutil
+
+import (
+	"reflect"
+)
+
+func Convert[T any](source interface{}) (target T) {
+	sourceValue := reflect.Indirect(reflect.ValueOf(source))
+
+	targetType := reflect.TypeOf(target)
+	targetValue := reflect.New(targetType).Elem()
+
+	for i := 0; i < sourceValue.NumField(); i++ {
+		sourceField := sourceValue.Type().Field(i)
+		if targetField, exist := targetType.FieldByName(sourceField.Name); exist {
+			if sourceField.Type == targetField.Type && sourceField.Type.Kind() != reflect.Struct {
+				targetValue.FieldByName(targetField.Name).Set(sourceValue.Field(i))
+			}
+		}
+	}
+	return targetValue.Interface().(T)
+}
